a27f0d83b2eb7a87fda54cf1c26c2dc212c67733
Mbitaze-Martine <mbitazem@gmail.com>
2025-10-07 21:46:53 +0100

    sample_script.py

--- sample_script.py
+++ sample_script.py
@@ -1,1 +1,9 @@
+# sample_script.py
+def sample_function():
+    """
+    Sample placeholder function for the repo.
+    """
+    return "This is a sample script."
 
+if __name__ == "__main__":
+    print(sample_function())
